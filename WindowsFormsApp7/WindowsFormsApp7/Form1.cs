48d8e9fdd634745dd443b4ee08852f2c484c6dc8
IgorGolyak <golyak.03@inbox.ru>
2021-11-13 09:48:24 +0700

    Prakticum3

---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        int pr = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void стартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void стопToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void сбросToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pr = 0;
+            progressBar1.Value = pr;
+            toolStripStatusLabel1.Text = pr + "%";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pr = pr + 1;
+            progressBar1.Value = pr;
+            toolStripStatusLabel1.Text=pr+"%";
+        }
+    }
+}
